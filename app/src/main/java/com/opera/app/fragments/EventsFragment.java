6079ef307cd503bc23a7b1217ac62855c5b75f93
1000779 <shivaprakash.patil@3i-infotech.com>
2018-03-22 16:19:08 +0530

    SHivaprakash: Fragments are created.

---
+++ app/src/main/java/com/opera/app/fragments/EventsFragment.java
@@ -0,0 +1,26 @@
+package com.opera.app.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.opera.app.R;
+
+public class EventsFragment extends BaseFragment{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_events, container, false);
+
+        return view;
+    }
+}
