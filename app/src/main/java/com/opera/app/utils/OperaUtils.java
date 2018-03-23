b51b52268810330d449686dad87cbfb9fd6243ca
1000632 <Shivaprakash.Patil@3i-infotech.com>
2018-03-23 12:49:13 +0530

    test committ

--- app/src/main/java/com/opera/app/utils/OperaUtils.java
+++ app/src/main/java/com/opera/app/utils/OperaUtils.java
@@ -1,5 +1,9 @@
 package com.opera.app.utils;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by 1000779 on 2/3/2018.
  */
@@ -13,5 +17,20 @@ public class OperaUtils {
     public static String FONT_MONSTERRAT_REGULAR = "Montserrat-Regular.ttf";
 
     public static String FONT_MONSTERRAT_BOLD = "Montserrat-Bold.ttf";
+
+    private void StoreInSharedPreference(Activity mActivity, String mKey, String mValue) {
+        SharedPreferences mPrefs = mActivity.getSharedPreferences("OperaData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor mEditor = mPrefs.edit();
+        mEditor.putString(mKey, mValue);
+        mEditor.commit();
+    }
+
+    private String GetSharedPreferences(Activity mActivity, String mKey, String mDefauleValue) {
+        String mValue = "";
+        SharedPreferences mPrefs = mActivity.getSharedPreferences("OperaData", Context.MODE_PRIVATE);
+        mValue = mPrefs.getString(mKey, mDefauleValue);
+
+        return mValue;
+    }
 
 }
