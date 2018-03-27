96731cd9c175b39e347d5aee70d849ac64360d5e
58001 <Shivaprakash.Patil@3i-infotech.com>
2018-03-27 15:28:10 +0530

    Prachi- Profile Activity

---
+++ app/src/main/java/com/opera/app/activities/MyProfileActivity.java
@@ -0,0 +1,61 @@
+package com.opera.app.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.opera.app.BaseActivity;
+import com.opera.app.R;
+import com.opera.app.utils.OperaUtils;
+
+import butterknife.BindView;
+
+/**
+ * Created by 58001 on 27-03-2018.
+ */
+
+public class MyProfileActivity extends BaseActivity {
+
+    private Activity mActivity;
+    private OperaUtils mOperaUtils = new OperaUtils();
+
+    @BindView(R.id.toolbar_setting)
+    Toolbar toolbar;
+
+    @BindView(R.id.img_back_arrow)
+    View inc_set_toolbar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mActivity = MyProfileActivity.this;
+
+        //For Language setting
+        mOperaUtils.CommonLanguageFunction(mActivity);
+        setContentView(R.layout.setting);
+
+        initToolbar();
+        initView();
+    }
+
+    private void initToolbar() {
+        setSupportActionBar(toolbar);
+    }
+
+    private void initView() {
+
+        inc_set_toolbar.findViewById(R.id.img_back_arrow).setVisibility(View.VISIBLE);
+        inc_set_toolbar.findViewById(R.id.img_back_arrow).setOnClickListener(backPress);
+    }
+
+    private View.OnClickListener backPress = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            onBackPressed();
+        }
+    };
+
+}
