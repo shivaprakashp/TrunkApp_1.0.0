749efa4569e535befd69a8413c8ecfd8ae9113e0
58001 <Shivaprakash.Patil@3i-infotech.com>
2018-03-27 12:52:53 +0530

    Prachi Commit for setting page changes

--- app/src/main/java/com/opera/app/activities/SettingsActivity.java
+++ app/src/main/java/com/opera/app/activities/SettingsActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import com.opera.app.BaseActivity;
 import com.opera.app.R;
@@ -16,10 +17,19 @@ import butterknife.BindView;
  * Created by 58001 on 23-03-2018.
  */
 
-public class SettingsActivity extends BaseActivity {
+public class SettingsActivity extends BaseActivity implements View.OnClickListener{
 
     private Activity mActivity;
     private OperaUtils mOperaUtils=new OperaUtils();
+
+    @BindView(R.id.englishSwitch)
+    TextView englishSwitch;
+
+    @BindView(R.id.arabicSwitch)
+    TextView arabicSwitch;
+
+    @BindView(R.id.tvLogout)
+    TextView tvLogout;
 
     @BindView(R.id.toolbar_setting)
     Toolbar toolbar;
@@ -49,6 +59,31 @@ public class SettingsActivity extends BaseActivity {
     }
 
     private void initView() {
+        //textview
+        englishSwitch.setOnClickListener(this);
+        arabicSwitch.setOnClickListener(this);
+        tvLogout.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.englishSwitch: {
+                englishSwitch.setBackgroundColor(getResources().getColor(R.color.colorBurgendy));
+                arabicSwitch.setBackgroundColor(getResources().getColor(R.color.dark_gray));
+                }
+                break;
+
+            case R.id.arabicSwitch:{
+                englishSwitch.setBackgroundColor(getResources().getColor(R.color.dark_gray));
+                arabicSwitch.setBackgroundColor(getResources().getColor(R.color.colorBurgendy));
+                }
+                break;
+
+            case R.id.tvLogout:
+
+                break;
+        }
     }
 }
