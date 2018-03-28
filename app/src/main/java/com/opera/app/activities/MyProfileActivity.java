a9e1acba5ead16780fa695a9b14e64ab950a61d5
58001 <Shivaprakash.Patil@3i-infotech.com>
2018-03-28 11:42:38 +0530

    Prachi- Profile Activity (Part 2)

--- app/src/main/java/com/opera/app/activities/MyProfileActivity.java
+++ app/src/main/java/com/opera/app/activities/MyProfileActivity.java
@@ -3,12 +3,20 @@ package com.opera.app.activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 
 import com.opera.app.BaseActivity;
 import com.opera.app.R;
+import com.opera.app.fragments.LoyaltyPointsFragment;
+import com.opera.app.fragments.ProfileFragment;
 import com.opera.app.utils.OperaUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 
@@ -21,11 +29,11 @@ public class MyProfileActivity extends BaseActivity {
     private Activity mActivity;
     private OperaUtils mOperaUtils = new OperaUtils();
 
-    @BindView(R.id.toolbar_setting)
-    Toolbar toolbar;
+    @BindView(R.id.tabhost)
+    TabLayout mTabHost;
 
-    @BindView(R.id.img_back_arrow)
-    View inc_set_toolbar;
+    @BindView(R.id.viewpager)
+    ViewPager mViewPager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,27 +43,52 @@ public class MyProfileActivity extends BaseActivity {
 
         //For Language setting
         mOperaUtils.CommonLanguageFunction(mActivity);
-        setContentView(R.layout.setting);
+        setContentView(R.layout.activity_my_profile);
 
-        initToolbar();
         initView();
     }
 
-    private void initToolbar() {
-        setSupportActionBar(toolbar);
-    }
 
     private void initView() {
 
-        inc_set_toolbar.findViewById(R.id.img_back_arrow).setVisibility(View.VISIBLE);
-        inc_set_toolbar.findViewById(R.id.img_back_arrow).setOnClickListener(backPress);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        // Add Fragments to adapter one by one
+        adapter.addFragment(new ProfileFragment(), getResources().getString(R.string.tab_profile));
+        adapter.addFragment(new LoyaltyPointsFragment(), getResources().getString(R.string.tab_loyalty));
+        mViewPager.setAdapter(adapter);
+
+        mTabHost.setupWithViewPager(mViewPager);
     }
 
-    private View.OnClickListener backPress = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            onBackPressed();
-        }
-    };
 
+    // Adapter for the viewpager using FragmentPagerAdapter
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
 }
