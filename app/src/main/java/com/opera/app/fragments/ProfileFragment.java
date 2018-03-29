c888de368e22d7154600dc0a95bc6b1575ad7969
58001 <Shivaprakash.Patil@3i-infotech.com>
2018-03-29 15:53:24 +0530

    Prachi- Edit Profile Changes

--- app/src/main/java/com/opera/app/fragments/ProfileFragment.java
+++ app/src/main/java/com/opera/app/fragments/ProfileFragment.java
@@ -1,14 +1,18 @@
 package com.opera.app.fragments;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.opera.app.R;
 import com.opera.app.utils.LanguageManager;
-import com.opera.app.utils.OperaUtils;
 
 /**
  * Created by 58001 on 27-03-2018.
@@ -33,5 +37,23 @@ public class ProfileFragment extends BaseFragment {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         return view;
+    }
+
+    public void showDialog() {
+        final Dialog dialog = new Dialog(mActivity);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(true);
+        dialog.setCanceledOnTouchOutside(true);
+
+        //For Language setting
+        LanguageManager.createInstance().CommonLanguageFunction(mActivity);
+        dialog.setContentView(R.layout.popup_changepassword);
+
+        TextView tv_forgotPassword = (TextView) dialog.findViewById(R.id.tv_forgotPassword);
+        EditText et_username = (EditText) dialog.findViewById(R.id.et_username);
+        Button btnSend = (Button) dialog.findViewById(R.id.btnSend);
+
+        dialog.show();
+
     }
 }
