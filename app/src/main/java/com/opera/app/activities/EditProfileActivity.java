7428bc3549799f886a93d92d4e5beb05895f7185
58001 <Shivaprakash.Patil@3i-infotech.com>
2018-04-03 17:45:41 +0530

    Prachi- Edit Profile and Change Password Changes

---
+++ app/src/main/java/com/opera/app/activities/EditProfileActivity.java
@@ -0,0 +1,330 @@
+package com.opera.app.activities;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.text.InputType;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.opera.app.BaseActivity;
+import com.opera.app.R;
+import com.opera.app.customwidget.EditTextWithFont;
+import com.opera.app.utils.LanguageManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+import butterknife.BindView;
+
+/**
+ * Created by 58001 on 03-04-2018.
+ */
+
+public class EditProfileActivity extends BaseActivity implements View.OnClickListener {
+
+    private Activity mActivity;
+    public static EditTextWithFont edtDob;
+
+    @BindView(R.id.btnCancel)
+    Button mBtnCancel;
+
+    @BindView(R.id.btnSave)
+    Button mBtnSave;
+
+    @BindView(R.id.edit_edtEmail)
+    View edit_edtEmail;
+
+    @BindView(R.id.edit_edtFirstName)
+    View edit_edtFirstName;
+
+    @BindView(R.id.edit_edtLastName)
+    View edit_edtLastName;
+
+    @BindView(R.id.edit_edtDob)
+    View edit_edtDob;
+
+    @BindView(R.id.edit_edtMobile)
+    View edit_edtMobile;
+
+    @BindView(R.id.edit_edtCity)
+    View edit_edtCity;
+
+    @BindView(R.id.edit_edtAddress)
+    View edit_edtAddress;
+
+    @BindView(R.id.spinnerNationality)
+    Spinner spinnerNationality;
+
+    @BindView(R.id.spinnerState)
+    Spinner spinnerState;
+
+    @BindView(R.id.spinnerCountry)
+    Spinner spinnerCountry;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mActivity = EditProfileActivity.this;
+
+        //For Language setting
+        LanguageManager.createInstance().CommonLanguageFunction(mActivity);
+        setContentView(R.layout.activity_edit_password);
+
+        initView();
+    }
+
+    private void initView() {
+
+        //button
+        mBtnSave.setOnClickListener(this);
+        mBtnCancel.setOnClickListener(this);
+        edit_edtDob.setOnClickListener(this);
+
+        //edittext
+        EditTextWithFont edtEmail = (EditTextWithFont) edit_edtEmail.findViewById(R.id.edt);
+        edtEmail.setHint(getString(R.string.edit_email));
+
+        EditTextWithFont edtFirstName = (EditTextWithFont) edit_edtFirstName.findViewById(R.id.edt);
+        edtFirstName.setHint(getString(R.string.edit_firstname));
+
+        EditTextWithFont edtLastName = (EditTextWithFont) edit_edtLastName.findViewById(R.id.edt);
+        edtLastName.setHint(getString(R.string.edit_lastname));
+
+        edtDob = (EditTextWithFont) edit_edtDob.findViewById(R.id.edt);
+        edtDob.setHint(getString(R.string.edit_dob));
+        edtDob.setFocusable(false);
+
+        EditTextWithFont edtMobile = (EditTextWithFont) edit_edtMobile.findViewById(R.id.edt);
+        edtMobile.setHint(getString(R.string.edit_mobile));
+
+        EditTextWithFont edtCity = (EditTextWithFont) edit_edtCity.findViewById(R.id.edt);
+        edtCity.setHint(getString(R.string.edit_city));
+
+        EditTextWithFont edtAddress = (EditTextWithFont) edit_edtAddress.findViewById(R.id.edt);
+        edtAddress.setHint(getString(R.string.edit_address));
+        edtAddress.setInputType(InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+        edtAddress.setSingleLine(false);
+
+        //---------------Nationality----------------
+        // Initializing a String Array
+        String[] nationality_str = new String[]{
+                getResources().getString(R.string.edit_nationality)
+        };
+        final List<String> List = new ArrayList<>(Arrays.asList(nationality_str));
+
+        // Initializing an ArrayAdapter
+        final ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(
+                this, R.layout.custom_spinner, List) {
+            @Override
+            public boolean isEnabled(int position) {
+                if (position == 0) {
+                    // Disable the first item from Spinner
+                    // First item will be use for hint
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if (position == 0) {
+                    // Set the hint text color gray
+                    tv.setTextColor(Color.GRAY);
+                } else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
+        spinnerArrayAdapter.setDropDownViewResource(R.layout.custom_spinner);
+
+        spinnerNationality.setAdapter(spinnerArrayAdapter);
+        spinnerNationality.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItemText = (String) parent.getItemAtPosition(position);
+                if (position > 0) {
+                    // Notify the selected item text
+                    /*Toast.makeText
+                            (getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT)
+                            .show();*/
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        //---------------State----------------
+        String[] state_str = new String[]{
+                getResources().getString(R.string.edit_state)
+        };
+        final List<String> stateList = new ArrayList<>(Arrays.asList(state_str));
+
+        // Initializing an ArrayAdapter
+        final ArrayAdapter<String> spinnerArrayAdapter1 = new ArrayAdapter<String>(
+                this, R.layout.custom_spinner, stateList) {
+            @Override
+            public boolean isEnabled(int position) {
+                if (position == 0) {
+                    // Disable the first item from Spinner
+                    // First item will be use for hint
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if (position == 0) {
+                    // Set the hint text color gray
+                    tv.setTextColor(Color.GRAY);
+                } else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
+        spinnerArrayAdapter1.setDropDownViewResource(R.layout.custom_spinner);
+
+        spinnerState.setAdapter(spinnerArrayAdapter1);
+        spinnerState.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItemText = (String) parent.getItemAtPosition(position);
+                if (position > 0) {
+                    // Notify the selected item text
+                    /*Toast.makeText
+                            (getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT)
+                            .show();*/
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        //---------------Country----------------
+        String[] country_str = new String[]{
+                getResources().getString(R.string.edit_country)
+        };
+        final List<String> countryList = new ArrayList<>(Arrays.asList(country_str));
+
+        // Initializing an ArrayAdapter
+        final ArrayAdapter<String> spinnerArrayAdapter2 = new ArrayAdapter<String>(
+                this, R.layout.custom_spinner, countryList) {
+            @Override
+            public boolean isEnabled(int position) {
+                if (position == 0) {
+                    // Disable the first item from Spinner
+                    // First item will be use for hint
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if (position == 0) {
+                    // Set the hint text color gray
+                    tv.setTextColor(Color.GRAY);
+                } else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
+        spinnerArrayAdapter2.setDropDownViewResource(R.layout.custom_spinner);
+
+        spinnerCountry.setAdapter(spinnerArrayAdapter2);
+        spinnerCountry.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItemText = (String) parent.getItemAtPosition(position);
+                if (position > 0) {
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+
+        edtDob.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+//your code
+                    DialogFragment newFragment = new RegisterActivity.DatePickerFragment();
+                    newFragment.show(getSupportFragmentManager(), "datePicker");
+
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnSave:
+                break;
+
+            case R.id.btnCancel:
+                onBackPressed();
+                break;
+
+        }
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            DatePickerDialog dialog = new DatePickerDialog(getActivity(), this, year, month, day);
+//            dialog.getDatePicker().setMaxDate(c.getTimeInMillis());
+            return dialog;
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            month = month + 1;
+            edtDob.setText(year + "-" + month + "-" + day);
+        }
+    }
+}
--- app/src/main/java/com/opera/app/fragments/ProfileFragment.java
+++ app/src/main/java/com/opera/app/fragments/ProfileFragment.java
@@ -2,6 +2,7 @@ package com.opera.app.fragments;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,10 +11,12 @@ import android.view.Window;
 import android.widget.Button;
 
 import com.opera.app.R;
+import com.opera.app.activities.EditProfileActivity;
 import com.opera.app.customwidget.EditTextWithFont;
 import com.opera.app.utils.LanguageManager;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by 58001 on 27-03-2018.
@@ -22,21 +25,13 @@ import butterknife.BindView;
 public class ProfileFragment extends BaseFragment implements View.OnClickListener {
 
     private Activity mActivity;
+    private Intent in;
 
     @BindView(R.id.btnEditProfile)
-    Button btnEditProfile;
+    Button mBtnEditProfile;
 
     @BindView(R.id.btnChangePassword)
-    Button btnChangePassword;
-
-    @BindView(R.id.prof_edtCurrPass)
-    View prof_edtCurrPass;
-
-    @BindView(R.id.prof_edtNewPass)
-    View prof_edtNewPass;
-
-    @BindView(R.id.prof_edtRePass)
-    View prof_edtRePass;
+    Button mBtnChangePassword;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,27 +46,29 @@ public class ProfileFragment extends BaseFragment implements View.OnClickListene
         //For Language setting
         LanguageManager.createInstance().CommonLanguageFunction(mActivity);
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        ButterKnife.bind(this, view);
 
-        //initView();
+        initView();
 
         return view;
     }
 
     private void initView() {
         //button
-        btnEditProfile.setOnClickListener(this);
-        btnChangePassword.setOnClickListener(this);
+        mBtnEditProfile.setOnClickListener(this);
+        mBtnChangePassword.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnEditProfile:
-
+                in = new Intent(getActivity(), EditProfileActivity.class);
+                startActivity(in);
                 break;
 
             case R.id.btnChangePassword:
-                //showDialog();
+                showDialog();
                 break;
         }
     }
@@ -87,13 +84,26 @@ public class ProfileFragment extends BaseFragment implements View.OnClickListene
         dialog.setContentView(R.layout.popup_changepassword);
 
         //EditText
-        EditTextWithFont tv_forgotPassword = (EditTextWithFont) dialog.findViewById(R.id.tv_forgotPassword);
-        EditTextWithFont et_username = (EditTextWithFont) dialog.findViewById(R.id.et_username);
-        EditTextWithFont et_username1 = (EditTextWithFont) dialog.findViewById(R.id.et_username);
+        EditTextWithFont edtCurrentPassword = (EditTextWithFont) dialog.findViewById(R.id.edtCurrentPassword);
+        EditTextWithFont edtNewPassword = (EditTextWithFont) dialog.findViewById(R.id.edtNewPassword);
+        EditTextWithFont edtConfNewPassword = (EditTextWithFont) dialog.findViewById(R.id.edtConfNewPassword);
 
         //Button
-        Button btnCancel = (Button) dialog.findViewById(R.id.btnCancel);
         Button btnSave = (Button) dialog.findViewById(R.id.btnSave);
+        Button btnCancel = (Button) dialog.findViewById(R.id.btnCancel);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
 
         dialog.show();
 
