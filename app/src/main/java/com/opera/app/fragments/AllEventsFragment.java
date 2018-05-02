f805fe1b624802177529991d01f567f8c02cdb92
58001 <Shivaprakash.Patil@3i-infotech.com>
2018-05-02 13:02:34 +0530

    Prachi - Events Tab generation

---
+++ app/src/main/java/com/opera/app/fragments/AllEventsFragment.java
@@ -0,0 +1,32 @@
+package com.opera.app.fragments;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.opera.app.R;
+import com.opera.app.utils.LanguageManager;
+
+public class AllEventsFragment extends BaseFragment {
+
+    private Activity mActivity;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mActivity = getActivity();
+        //For Language setting
+        LanguageManager.createInstance().CommonLanguageFunction(mActivity);
+        View view = inflater.inflate(R.layout.fragment_all_events, container, false);
+
+        return view;
+    }
+}
