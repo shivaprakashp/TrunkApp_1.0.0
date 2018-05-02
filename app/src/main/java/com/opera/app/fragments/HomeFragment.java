6bdcd3230fd47e7b36c023cc4e09c4eaf69597c1
1000632 <Shivaprakash.Patil@3i-infotech.com>
2018-05-02 12:57:34 +0530

    Home fragment

--- app/src/main/java/com/opera/app/fragments/HomeFragment.java
+++ app/src/main/java/com/opera/app/fragments/HomeFragment.java
@@ -7,12 +7,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.opera.app.R;
+import com.opera.app.listadapters.CoverFlowAdapter;
+import com.opera.app.pojo.GameEntity;
 import com.opera.app.utils.LanguageManager;
 import com.opera.app.utils.OperaUtils;
+
+import java.util.ArrayList;
+
+import it.moondroid.coverflow.components.ui.containers.FeatureCoverFlow;
 
 public class HomeFragment extends BaseFragment {
 
     private Activity mActivity;
+    private FeatureCoverFlow mCoverFlow;
+    private CoverFlowAdapter mAdapter;
+    private ArrayList<GameEntity> mData = new ArrayList<>(0);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,16 @@ public class HomeFragment extends BaseFragment {
         //For Language setting
         LanguageManager.createInstance().CommonLanguageFunction(mActivity);
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        mData.add(new GameEntity(R.drawable.ic_profile_bg, R.string.empty));
+        mData.add(new GameEntity(R.drawable.bg_login, R.string.empty));
+        mData.add(new GameEntity(R.drawable.ic_profile_bg, R.string.empty));
+        mData.add(new GameEntity(R.drawable.bg_login, R.string.empty));
+
+        mAdapter = new CoverFlowAdapter(getActivity());
+        mAdapter.setData(mData);
+        mCoverFlow = (FeatureCoverFlow) view.findViewById(R.id.coverflow);
+        mCoverFlow.setAdapter(mAdapter);
 
         return view;
     }
---
+++ app/src/main/java/com/opera/app/pojo/GameEntity.java
@@ -0,0 +1,15 @@
+package com.opera.app.pojo;
+
+/**
+ * Created by 1000632 on 2/19/2018.
+ */
+
+public class GameEntity {
+    public int imageResId;
+    public int titleResId;
+
+    public GameEntity(int imageResId, int titleResId){
+        this.imageResId = imageResId;
+        this.titleResId = titleResId;
+    }
+}
