f0a8a4aec4382937d0131f62da891c7908540cf9
58001 <Shivaprakash.Patil@3i-infotech.com>
2018-05-02 15:56:50 +0530

    Prachi - Issue Fixed

--- app/src/main/java/com/opera/app/dialogues/ErrorDialogue.java
+++ app/src/main/java/com/opera/app/dialogues/ErrorDialogue.java
@@ -2,13 +2,14 @@ package com.opera.app.dialogues;
 
 import android.app.Activity;
 import android.app.Dialog;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.View;
 import android.view.Window;
 
+import com.opera.app.BaseActivity;
 import com.opera.app.R;
+import com.opera.app.activities.LoginActivity;
 import com.opera.app.customwidget.ButtonWithFont;
 import com.opera.app.customwidget.TextViewWithFont;
 
@@ -17,8 +18,9 @@ import butterknife.ButterKnife;
 
 public class ErrorDialogue extends Dialog {
 
-    private Context context;
+    private Activity mActivity;
     private String message;
+    private BaseActivity mBaseActivity;
 
     @BindView(R.id.txtErrorMessage)
     TextViewWithFont txtErrorMessage;
@@ -26,11 +28,12 @@ public class ErrorDialogue extends Dialog {
     @BindView(R.id.btnError)
     ButtonWithFont btnError;
 
-    public ErrorDialogue(@NonNull Context context, String message) {
-        super(context);
+    public ErrorDialogue(@NonNull Activity mActivity, String message) {
+        super(mActivity);
 
-        this.context = context;
+        this.mActivity = mActivity;
         this.message = message;
+        mBaseActivity = (BaseActivity) mActivity;
     }
 
     @Override
@@ -47,8 +50,12 @@ public class ErrorDialogue extends Dialog {
     private View.OnClickListener buttonClose = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            dismiss();
-
+            if (message.contains("Account already available.")) {
+                mBaseActivity.openActivity(mActivity, LoginActivity.class);
+                dismiss();
+            } else {
+                dismiss();
+            }
         }
     };
 }
