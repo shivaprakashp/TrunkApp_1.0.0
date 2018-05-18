f0c72bca2683ebc09a22bd88c35ddb3aa9094478
1000632 <Shivaprakash.Patil@3i-infotech.com>
2018-05-18 15:54:56 +0530

    EventDetails page

--- app/src/main/java/com/opera/app/pojo/events/eventlisiting/EventTime.java
+++ app/src/main/java/com/opera/app/pojo/events/eventlisiting/EventTime.java
@@ -1,13 +1,20 @@
 package com.opera.app.pojo.events.eventlisiting;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by 1000632 on 5/9/2018.
  */
 
 public class EventTime
 {
+    @SerializedName("FromTime")
+    @Expose
     private String FromTime;
 
+    @SerializedName("ToTime")
+    @Expose
     private String ToTime;
 
     public String getFromTime() {
