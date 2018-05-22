b36804e31ac48a98bd0becefa51ad735b608d41d
58001 <Shivaprakash.Patil@3i-infotech.com>
2018-05-22 18:07:13 +0530

    Prachi - Promotion Mock api integration and Dubai Opera tour

--- app/src/main/java/com/opera/app/database/OperaDBHandler.java
+++ app/src/main/java/com/opera/app/database/OperaDBHandler.java
@@ -8,6 +8,7 @@ import com.opera.app.database.events.EventDetailsDB;
 import com.opera.app.database.events.EventGenresDB;
 import com.opera.app.database.events.EventListingDB;
 import com.opera.app.database.notification.NotificationDetailsDB;
+import com.opera.app.database.notification.PromotionDetailsDB;
 import com.opera.app.database.restaurants.DatabaseHelper;
 import com.opera.app.database.restaurants.SeanRestOpeation;
 
@@ -29,6 +30,7 @@ public class OperaDBHandler extends SQLiteOpenHelper {
         db.execSQL(EventDetailsDB.CREATE_TABLE_EVENT_DETAILS);
         db.execSQL(EventGenresDB.CREATE_TABLE_GENRES_LISTING);
         db.execSQL(NotificationDetailsDB.CREATE_TABLE_NOTIFICATION);
+        db.execSQL(PromotionDetailsDB.CREATE_TABLE_PROMOTION);
     }
 
     @Override
@@ -40,6 +42,7 @@ public class OperaDBHandler extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + EventDetailsDB.TABLE_EVENT_DETAILS);
         db.execSQL("DROP TABLE IF EXISTS " + EventGenresDB.TABLE_GENRES_LISTING);
         db.execSQL("DROP TABLE IF EXISTS " + NotificationDetailsDB.TABLE_NOTIFICATION_DETAILS);
+        db.execSQL("DROP TABLE IF EXISTS " + PromotionDetailsDB.TABLE_PROMOTION_DETAILS);
         onCreate(db);
     }
 
