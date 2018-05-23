85cadb56edf1eea42013325ff5abf44b1cc62507
INFOTECH\1000779 <Shivaprakash.Patil@3i-infotech.com>
2018-05-23 13:29:25 +0530

    Shivaprakash: Infobip integrated

--- app/src/main/java/com/opera/app/BaseActivity.java
+++ app/src/main/java/com/opera/app/BaseActivity.java
@@ -1,13 +1,18 @@
 package com.opera.app;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.opera.app.customwidget.CustomToast;
 
+import org.infobip.mobile.messaging.Event;
 import org.infobip.mobile.messaging.Message;
 import org.infobip.mobile.messaging.MobileMessaging;
 import org.infobip.mobile.messaging.storage.MessageStore;
--- app/src/main/java/com/opera/app/MainApplication.java
+++ app/src/main/java/com/opera/app/MainApplication.java
@@ -34,19 +34,7 @@ public class MainApplication extends Application {
 
         setFont();
         initDagger();
-        initNotification();
-    }
 
-    private void initNotification(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            // Create channel to show notifications.
-            String channelId  = getString(R.string.default_notification_channel_id);
-            String channelName = getString(R.string.app_name);
-            NotificationManager notificationManager =
-                    getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(new NotificationChannel(channelId,
-                    channelName, NotificationManager.IMPORTANCE_LOW));
-        }
     }
 
     private void setFont(){
