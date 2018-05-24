a33478dab99fab7a1372ea8c443a76fc5f9aae7c
1000632 <Shivaprakash.Patil@3i-infotech.com>
2018-05-24 17:42:04 +0530

    Merge branch 'master' of https://bitbucket.org/dubaioperateam/androidapprepository
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/opera/app/listadapters/RestaurantAdapter.java
    #	app/src/main/java/com/opera/app/listadapters/WhatsOnPagerAdapter.java
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/opera/app/listadapters/RestaurantAdapter.java
+++ app/src/main/java/com/opera/app/listadapters/RestaurantAdapter.java
@@ -1,6 +1,7 @@
 package com.opera.app.listadapters;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
