63a3f25b833b33a9f7083897eb315c12754c15b5
1000632 <Shivaprakash.Patil@3i-infotech.com>
2018-06-01 18:33:30 +0530

    Event detail and restaurant gradient repair

--- app/src/main/java/com/opera/app/fragments/wallet/CompletedWalletFragment.java
+++ app/src/main/java/com/opera/app/fragments/wallet/CompletedWalletFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.opera.app.R;
 import com.opera.app.customwidget.TextViewWithFont;
@@ -21,6 +22,7 @@ public class CompletedWalletFragment extends Fragment {
 
     private TodayWalletView walletView;
     private TextViewWithFont txtWalletEventTitle;
+    private TextView txt_testing;
     private LinearLayout linearParent;
 
     public CompletedWalletFragment() {
@@ -36,6 +38,7 @@ public class CompletedWalletFragment extends Fragment {
 
         linearParent = (LinearLayout) view.findViewById(R.id.linearParent);
         txtWalletEventTitle = (TextViewWithFont) view.findViewById(R.id.txtWalletEventTitle);
+        txt_testing = (TextView) view.findViewById(R.id.txt_testing);
         walletView = (TodayWalletView) view.findViewById(R.id.helperWalletEvent);
 
         int mTotalData = 0;
@@ -51,9 +54,11 @@ public class CompletedWalletFragment extends Fragment {
         if (mTotalData > 0) {
             linearParent.setVisibility(View.VISIBLE);
             txtWalletEventTitle.setVisibility(View.GONE);
+            txt_testing.setVisibility(View.GONE);
         } else {
             linearParent.setVisibility(View.GONE);
             txtWalletEventTitle.setVisibility(View.VISIBLE);
+            txt_testing.setVisibility(View.GONE);
         }
 
         return view;
