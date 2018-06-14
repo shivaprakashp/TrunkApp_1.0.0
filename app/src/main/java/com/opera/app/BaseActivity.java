4a631dcb86d78e0ad55b4b237d413f975fd1683d
INFOTECH\1000779 <Shivaprakash.Patil@3i-infotech.com>
2018-06-14 16:50:04 +0530

    Shivaprakash: Infobip data added

--- app/src/main/java/com/opera/app/BaseActivity.java
+++ app/src/main/java/com/opera/app/BaseActivity.java
@@ -2,25 +2,16 @@ package com.opera.app;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.opera.app.customwidget.CustomToast;
 
-import org.infobip.mobile.messaging.Event;
-import org.infobip.mobile.messaging.MobileMessaging;
-import org.infobip.mobile.messaging.NotificationSettings;
 import org.infobip.mobile.messaging.geo.MobileGeo;
-import org.infobip.mobile.messaging.geo.geofencing.Geofencing;
-import org.infobip.mobile.messaging.storage.MessageStore;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -38,41 +29,15 @@ public class BaseActivity extends AppCompatActivity {
     public SharedPreferences mSharedPreferences;
     public CustomToast customToast;
     private int prefMode = 0;
+
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
-    private MobileMessaging mobileMessaging;
-    private MessageStore messageStore;
-
-    private BroadcastReceiver messageReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-          //  String gcmRegistrationToken = intent.getStringExtra(BroadcastParameter.EXTRA_GCM_TOKEN);
-           // Log.i("regId", gcmRegistrationToken);
-
-        }
-    };
 
     @Override
     public void setContentView(int layoutResID) {
         super.setContentView(layoutResID);
         bindViews();
 
-       /* initInfoBip();
-        activateGeofencing();
-
-        LocalBroadcastManager
-                .getInstance(this)
-                .registerReceiver(messageReceiver, new IntentFilter(Event.MESSAGE_RECEIVED.getKey()));*/
-    }
-
-    private void initInfoBip(){
-
-        mobileMessaging = new MobileMessaging.Builder(getApplication()).withDisplayNotification(new NotificationSettings.Builder(this)
-                .withMultipleNotifications()
-                .withDefaultIcon(R.mipmap.ic_launcher)
-                .build())
-                .build();
-
-       /* Log.i("regId", mobileMessaging.getPushRegistrationId());*/
+        activateGeoFenc();
     }
 
     protected void bindViews() {
@@ -107,31 +72,33 @@ public class BaseActivity extends AppCompatActivity {
         return null;
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
-            activateGeofencing();
-        }
-    }
-
-    private void activateGeofencing() {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+    private void activateGeoFenc(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             String permissions[] = {Manifest.permission.ACCESS_FINE_LOCATION};
             ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
             return;
         }
 
         MobileGeo.getInstance(this).activateGeofencing();
-        Geofencing.getInstance(this).startGeoMonitoring();
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        MobileGeo.getInstance(this).activateGeofencing();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        LocalBroadcastManager
-                .getInstance(this)
-                .unregisterReceiver(messageReceiver);
+
     }
 }
