3d47a8db415c8bd41954f8003ab6aa119ca6f11b
INFOTECH\1000779 <Shivaprakash.Patil@3i-infotech.com>
2018-06-18 16:29:43 +0530

    Shivaprakash: completed the implementation part of infobip

--- app/src/main/java/com/opera/app/services/NotificationTappedReceiver.java
+++ app/src/main/java/com/opera/app/services/NotificationTappedReceiver.java
@@ -5,19 +5,37 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import com.opera.app.activities.MainActivity;
 import com.opera.app.activities.MyProfileActivity;
+import com.opera.app.activities.NotificationActivity;
 import com.opera.app.activities.PromotionsActivity;
 
 import org.infobip.mobile.messaging.Message;
 
+//on Tap notification Open the activity
 public class NotificationTappedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        //received data once notification reached
         Message message = Message.createFrom(intent.getExtras());
-        Log.i("message", message.toString());
-        Intent i = new Intent(context, PromotionsActivity.class);
-        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(i);
+        //received from infobip
+        //check custom payload has null value
+        if (message.getCustomPayload()!=null){
+            /*look out the notification type,
+            * whether notification type is of promotion or geo notification*/
+            if (message.getCustomPayload().opt("notifyType").toString().
+                    equalsIgnoreCase("promotion")){
+                //pass intent based on type of notification
+               intent  = new Intent(context, PromotionsActivity.class);
+            }else{
+                intent = new Intent(context, NotificationActivity.class);
+            }
+        }else{
+            intent = new Intent(context, MainActivity.class);
+        }
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
     }
 }
