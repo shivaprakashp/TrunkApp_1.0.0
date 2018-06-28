5076a1c54689aaebc8ff4e810ffdf1f7308e1f7b
1000632 <Shivaprakash.Patil@3i-infotech.com>
2018-06-28 12:01:21 +0530

    Left to right handling for numbers only

--- app/src/main/java/com/opera/app/activities/TestAct.java
+++ app/src/main/java/com/opera/app/activities/TestAct.java
@@ -1,6 +1,7 @@
 package com.opera.app.activities;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -12,14 +13,18 @@ import android.net.http.SslError;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Message;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.ClientCertRequest;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
 import android.webkit.HttpAuthHandler;
 import android.webkit.RenderProcessGoneDetail;
 import android.webkit.SslErrorHandler;
+import android.webkit.URLUtil;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
@@ -31,6 +36,7 @@ import android.widget.Button;
 import com.opera.app.R;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * Created by 1000632 on 4/20/2018.
@@ -38,8 +44,10 @@ import java.io.IOException;
 
 public class TestAct extends Activity {
 
-    private Button btnOpenApple, btnEmbedUrl, btnItunesUrl;
+    private Button btnOpenApple, btnEmbedUrl, btnItunesUrl,btnBuyTickers;
     private Activity activity;
+    private WebView webBuyTickets;
+    private ProgressDialog mDialog;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -47,10 +55,68 @@ public class TestAct extends Activity {
         setContentView(R.layout.test_act);
 
         activity = TestAct.this;
-
-        /*btnOpenApple = (Button) findViewById(R.id.btnOpenApple);
+        mDialog=new ProgressDialog(TestAct.this);
+        btnOpenApple = (Button) findViewById(R.id.btnOpenApple);
         btnEmbedUrl = (Button) findViewById(R.id.btnEmbedUrl);
-        btnItunesUrl = (Button) findViewById(R.id.btnItunesUrl);*/
+        btnItunesUrl = (Button) findViewById(R.id.btnItunesUrl);
+        btnBuyTickers = (Button) findViewById(R.id.btnBuyTickers);
+        webBuyTickets=(WebView)findViewById(R.id.webBuyTickets);
+
+        btnBuyTickers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDialog.show();
+                mDialog.setMessage("Please wait...");
+                btnBuyTickers.setVisibility(View.GONE);
+                webBuyTickets.setVisibility(View.VISIBLE);
+
+                CookieManager cookieManager = CookieManager.getInstance();
+                cookieManager.setCookie("https://dubaioperaw-mobile-uat.etixdubai.com/", "X-Auth-Token=6LfpK%2fuCFNVVVH5FDBzs03mcFvUiAUABbo6J6fxTWCwqe5KVLn0HHDL3clMDBO3ayg5woEOTr4%2fJuDtVmhYOTg%3d%3d");
+                webBuyTickets.loadUrl("https://dubaioperaw-mobile-uat.etixdubai.com/shows/show.aspx?sh=TEST2PC");
+                webBuyTickets.getSettings().setJavaScriptEnabled(true);
+                webBuyTickets.getSettings().setDomStorageEnabled(true);
+                webBuyTickets.setWebViewClient(new MyWebViewClient());
+                cookieManager.setAcceptThirdPartyCookies(webBuyTickets, true);
+                webBuyTickets.requestFocus();
+
+                /*webBuyTickets.getSettings().setJavaScriptEnabled(true);
+                webBuyTickets.getSettings().setSaveFormData(false);
+
+                CookieSyncManager.createInstance(TestAct.this);
+                CookieSyncManager.getInstance().startSync();
+                CookieManager cookieManager = CookieManager.getInstance();
+                cookieManager.setAcceptCookie(true);
+                CookieManager.getInstance().setAcceptThirdPartyCookies(webBuyTickets, true);
+
+                *//*SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+                String token2= mPreferences.getString("auth_token","");*//*
+
+                HashMap<String, String> map = new HashMap<String, String>();
+                map.put("x-auth-token", "G7%2blsuNjQxg1ZUrsh1yofoBwyZ84djnIKPRTpJzkjcRhBY%2bZUlySwpYeWa%2fGVKjF4sda8aPjlKXCQUs%2bYl2oaQ%3d%3d");
+
+                webBuyTickets.getSettings().setAppCacheEnabled(true);
+                webBuyTickets.setWebViewClient(new WebViewClient() {
+                    public boolean shouldOverrideUrlLoading(WebView view,String url) {
+                        view.loadUrl(url);
+                        return true;
+                    }
+                });
+                webBuyTickets.loadUrl("https://dubaioperaw-mobile-uat.etixdubai.com/shows/show.aspx?sh=TEST2PC", map);*/
+
+
+
+                /*String myURL = "https://dubaioperaw-mobile-uat.etixdubai.com/shows/show.aspx?sh=TEST2PC";
+
+                android.webkit.CookieManager cookieManager = android.webkit.CookieManager.getInstance();
+                cookieManager.setCookie("https://dubaioperaw-mobile-uat.etixdubai.com/", "X-Auth-Token=G7%2blsuNjQxg1ZUrsh1yofoBwyZ84djnIKPRTpJzkjcRhBY%2bZUlySwpYeWa%2fGVKjF4sda8aPjlKXCQUs%2bYl2oaQ%3d%3d");
+                cookieManager.setAcceptCookie(true);
+                cookieManager.acceptCookie();
+                cookieManager.setAcceptFileSchemeCookies(true);
+                cookieManager.getInstance().setAcceptCookie(true);
+                cookieManager.getCookie(myURL);
+                webBuyTickets.loadUrl(myURL);*/
+            }
+        });
 
         btnOpenApple.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,27 +165,35 @@ public class TestAct extends Activity {
         }
     }
 
-    public class myWebClient extends WebViewClient {
+    private class MyWebViewClient extends WebViewClient {
         @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            // TODO Auto-generated method stub
-            super.onPageStarted(view, url, favicon);
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+            return false;
         }
 
         @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            // TODO Auto-generated method stub
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            mDialog.dismiss();
+        }
 
-            view.loadUrl(url);
-            return true;
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            Log.e("PageStarted",url.toString());
+        }
+
+        @Override
+        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+            super.onReceivedError(view, request, error);
+
         }
 
         @Override
         public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-            //super.onReceivedSslError(view, handler, error);
-            handler.proceed();
+            super.onReceivedSslError(view, handler, error);
+
         }
-
-
     }
 }
