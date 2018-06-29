105b0347f3d63a6983752031807006f6818331db
1000632 <Shivaprakash.Patil@3i-infotech.com>
2018-06-29 14:24:04 +0530

    Search issue

--- app/src/main/java/com/opera/app/services/NotificationTappedReceiver.java
+++ app/src/main/java/com/opera/app/services/NotificationTappedReceiver.java
@@ -22,37 +22,36 @@ public class NotificationTappedReceiver extends BroadcastReceiver {
         Message message = Message.createFrom(intent.getExtras());
         //received from infobip
         //check custom payload has null value
-        if (message.getCustomPayload()!=null){
+        if (message.getCustomPayload() != null) {
             /*look out the notification type,
             * whether notification type is of promotion or geo notification*/
             if (message.getCustomPayload().opt("notifyType").toString().
-                    equalsIgnoreCase("promotion")){
+                    equalsIgnoreCase("promotion")) {
                 //pass intent based on type of notification
-               intent  = new Intent(context, PromotionsActivity.class);
-            }else{
-                if(message.getCustomPayload().opt("dataType").toString().equalsIgnoreCase("Restaurant")){
+                intent = new Intent(context, PromotionsActivity.class);
+            } else {
+                if (message.getCustomPayload().opt("dataType").toString().equalsIgnoreCase("Restaurant")) {
                     intent = new Intent(context, RestaurantCompleteDetails.class);
-                    intent.putExtra("RestaurantIdSiteCore",message.getCustomPayload().opt("dataKey").toString());
-                }else if(message.getCustomPayload().opt("dataType").toString().equalsIgnoreCase("Event")){
+                    intent.putExtra("RestaurantIdSiteCore", message.getCustomPayload().opt("dataKey").toString());
+                } else if (message.getCustomPayload().opt("dataType").toString().equalsIgnoreCase("Event")) {
 
                     EventListingDB mEventListingDB = new EventListingDB(context);
                     mEventListingDB.open();
 
                     intent = new Intent(context, EventDetailsActivity.class);
-                    intent.putExtra("EventId",message.getCustomPayload().opt("dataKey").toString());
+                    intent.putExtra("EventId", message.getCustomPayload().opt("dataKey").toString());
                     if (mEventListingDB.IsFavouriteForSpecificEvent(message.getCustomPayload().opt("dataKey").toString())) {
                         intent.putExtra("IsFavourite", "true");
                     } else {
                         intent.putExtra("IsFavourite", "false");
                     }
                     mEventListingDB.close();
-                }
-                else{
+                } else {
                     intent = new Intent(context, NotificationActivity.class);
                 }
 
             }
-        }else{
+        } else {
             intent = new Intent(context, MainActivity.class);
         }
 
